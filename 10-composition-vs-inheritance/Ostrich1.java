c5b17b957baf971ca46bb1a988c3c120eb0de503
chen <cty9388@hotmail.com>
2021-07-28 09:58:03 +0800

    create two class in 10

---
+++ 10-composition-vs-inheritance/Ostrich1.java
@@ -0,0 +1,12 @@
+public class Ostrich1 implements Tweetable, EggLayable {
+
+    @Override
+    public void tweet() {
+        //...
+    }
+
+    @Override
+    public void layEgg() {
+        //...
+    }
+}
---
+++ 10-composition-vs-inheritance/Sparrow1.java
@@ -0,0 +1,16 @@
+public class Sparrow1 implements Flyable, Tweetable, EggLayable {
+    @Override
+    public void fly() {
+        //...
+    }
+
+    @Override
+    public void tweet() {
+        //...
+    }
+
+    @Override
+    public void layEgg() {
+        //...
+    }
+}
