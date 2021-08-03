0e76cea22d1014dd5b15afb136ecbf12a5f82821
chen <cty9388@hotmail.com>
2021-08-03 18:36:18 +0800

    create virtualWalletBo.java

---
+++ 09-interface-implementation/VirtualWalletService.java
@@ -0,0 +1,58 @@
+
+public class VirtualWalletService {
+  // 通过构造函数或者IOC框架注入
+  private VirtualWalletRepository walletRepo;
+  private VirtualWalletTransactionRepository transactionRepo;
+  
+  public VirtualWalletBo getVirtualWallet(Long walletId) {
+    VirtualWalletEntity walletEntity = walletRepo.getWalletEntity(walletId);
+    VirtualWalletBo walletBo = convert(walletEntity);
+    return walletBo;
+  }
+  
+  public BigDecimal getBalance(Long walletId) {
+    return walletRepo.getBalance(walletId);
+  }
+
+  @Transactional
+  public void debit(Long walletId, BigDecimal amount) {
+    VirtualWalletEntity walletEntity = walletRepo.getWalletEntity(walletId);
+    BigDecimal balance = walletEntity.getBalance();
+    if (balance.compareTo(amount) < 0) {
+      throw new NoSufficientBalanceException(...);
+    }
+    VirtualWalletTransactionEntity transactionEntity = new VirtualWalletTransactionEntity();
+    transactionEntity.setAmount(amount);
+    transactionEntity.setCreateTime(System.currentTimeMillis());
+    transactionEntity.setType(TransactionType.DEBIT);
+    transactionEntity.setFromWalletId(walletId);
+    transactionRepo.saveTransaction(transactionEntity);
+    walletRepo.updateBalance(walletId, balance.subtract(amount));
+  }
+
+  @Transactional
+  public void credit(Long walletId, BigDecimal amount) {
+    VirtualWalletTransactionEntity transactionEntity = new VirtualWalletTransactionEntity();
+    transactionEntity.setAmount(amount);
+    transactionEntity.setCreateTime(System.currentTimeMillis());
+    transactionEntity.setType(TransactionType.CREDIT);
+    transactionEntity.setFromWalletId(walletId);
+    transactionRepo.saveTransaction(transactionEntity);
+    VirtualWalletEntity walletEntity = walletRepo.getWalletEntity(walletId);
+    BigDecimal balance = walletEntity.getBalance();
+    walletRepo.updateBalance(walletId, balance.add(amount));
+  }
+
+  @Transactional
+  public void transfer(Long fromWalletId, Long toWalletId, BigDecimal amount) {
+    VirtualWalletTransactionEntity transactionEntity = new VirtualWalletTransactionEntity();
+    transactionEntity.setAmount(amount);
+    transactionEntity.setCreateTime(System.currentTimeMillis());
+    transactionEntity.setType(TransactionType.TRANSFER);
+    transactionEntity.setFromWalletId(fromWalletId);
+    transactionEntity.setToWalletId(toWalletId);
+    transactionRepo.saveTransaction(transactionEntity);
+    debit(fromWalletId, amount);
+    credit(toWalletId, amount);
+  }
+}
